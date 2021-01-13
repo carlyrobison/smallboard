2684ee3b65979475ddae02329553fffb4222f650
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2021-01-13 01:58:17 -0500

    add link to google drive folder (#349)

--- hunts/src/HuntViewHeader.js
+++ hunts/src/HuntViewHeader.js
@@ -9,6 +9,7 @@ import {
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import ReactTooltip from "react-tooltip";
 
 function HuntViewHeader({ hunt }) {
   const dispatch = useDispatch();
@@ -17,6 +18,14 @@ function HuntViewHeader({ hunt }) {
   const numSolved = useSelector(selectNumSolved);
   const numUnsolved = useSelector(selectNumUnsolved);
   const numMetasSolved = useSelector(selectNumMetasSolved);
+  const driveRedirect = hunt.has_drive ? (
+    <div className="row">
+      <ReactTooltip place="bottom" />
+      <a data-tip="Create your drawings, jamboards, etc here" href={"drive"}>
+        Google Drive Folder
+      </a>
+    </div>
+  ) : null;
 
   return (
     <div>
@@ -27,9 +36,16 @@ function HuntViewHeader({ hunt }) {
           padding: "2px",
           alignItems: "center",
         }}
+        className="row justify-content-between"
       >
-        <h1>{hunt.name} - All Puzzles</h1>
-        <a href={"stats"}>Hunt Statistics</a>
+        <h1 className="col-10">{hunt.name} - All Puzzles</h1>
+
+        <div className="col-2">
+          <a className="row " href={"stats"}>
+            Hunt Statistics
+          </a>
+          {driveRedirect}
+        </div>
       </div>
       <Container fluid>
         <Row className="text-center font-weight-bold small">
